ee1b066f9057c7470cde4059c7a843d43f426fa3
Oliver Meyer <oliver.meyer@jondos.de>
2017-02-20 20:12:14 +0100

    Corrected case statement

--- tbb-builder/run-nodocker.sh
+++ tbb-builder/run-nodocker.sh
@@ -3,17 +3,17 @@
 
 cat Dockerfile | while read line ; do
 	case $line in
-		ENV TBB_BRANCH*)
-			export TBB_BRANCH=${line##* }
+		ENV*TBB_BRANCH*)
+			echo export TBB_BRANCH=${line##* }
 			;;
-		ENV TBB_TAG*)
-			export TBB_TAG=${line##* }
+		ENV*TBB_TAG*)
+			echo export TBB_TAG=${line##* }
 			;;
-		RUN apt-get*)
-			eval ${line#* }
+		RUN*apt-get*)
+			echo eval ${line#* }
 			;;
-		RUN mkdir*)
-			eval ${line#* }
+		RUN*mkdir*)
+			echo eval ${line#* }
 			;;
 	esac
 done
