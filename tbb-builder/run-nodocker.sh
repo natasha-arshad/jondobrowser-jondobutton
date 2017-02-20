570ab577afc74bed565b105580af3455481c8d1a
Oliver Meyer <oliver.meyer@jondos.de>
2017-02-20 20:08:27 +0100

    Added a script to run TorBrowser build without using Docker

---
+++ tbb-builder/run-nodocker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+cat Dockerfile | while read line ; do
+	case $line in
+		ENV TBB_BRANCH*)
+			export TBB_BRANCH=${line##* }
+			;;
+		ENV TBB_TAG*)
+			export TBB_TAG=${line##* }
+			;;
+		RUN apt-get*)
+			eval ${line#* }
+			;;
+		RUN mkdir*)
+			eval ${line#* }
+			;;
+	esac
+done
+
+. ./tbb-build.sh
