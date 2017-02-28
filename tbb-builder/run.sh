86e367ad1a421999e10e146ee2644e0c1ffe0378
Oliver <javadoc@email.de>
2017-02-28 22:24:14 +0100

    Externalized ENV into docker env file

--- tbb-builder/run.sh
+++ tbb-builder/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/tbb-builder
 sudo docker build --tag jondos-test/tbb-builder .
-sudo docker start -ai tbb-builder || sudo docker run --privileged -ti "$@" --name tbb-builder -v /mnt:/var/src jondos-test/tbb-builder
+sudo docker start -ai tbb-builder || sudo docker run --env-file docker.env --privileged -ti "$@" --name tbb-builder -v /mnt:/var/src jondos-test/tbb-builder
--- tbb-builder/tbb-build.sh
+++ tbb-builder/tbb-build.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 # This script is run inside the Docker container to prime the build for Tor Browser
 
-test -z $TBB_BRANCH && export TBB_BRANCH=$1
-test -z $TBB_TAG && export TBB_TAG=$2
+cd $(dirname $0)
+
+# The presence of a Dockerfile in the same directory as this script indicates that
+# we are not running in a container
+if [ -f Dockerfile ] ; then
+
+  echo "Not running in a Docker container."
+
+  cat Dockerfile | while read line ; do
+  	case $line in
+  		RUN*apt-get*)
+  			eval ${line#* }
+  			;;
+  		RUN*mkdir*)
+  			eval ${line#* }
+  			;;
+  	esac
+  done
+
+  if [ -f docker.env ] ; then
+    . docker.env
+  fi
+else
+  echo "Running inside Docker container."
+  export USE_LXC=1
+fi
 
 cd /var/src
 mkdir -p tor-browser-build
@@ -12,4 +36,7 @@ git clone -b $TBB_BRANCH https://git.torproject.org/builders/tor-browser-bundle.
 cd tor-browser-bundle
 git checkout $TBB_TAG
 cd gitian
-exec /bin/bash -l
+
+echo 'To build torbrowser, run "make TORSOCKS="'
+
+test -f Dockerfile || exec /bin/bash -l
