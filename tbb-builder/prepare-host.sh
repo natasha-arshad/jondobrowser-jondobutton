44d344052a499fbc12291b8208a84dde9bc8b6af
Oliver <javadoc@email.de>
2017-03-20 21:24:21 +0100

    removed PDF formatting symbols

--- tbb-builder/prepare-host.sh
+++ tbb-builder/prepare-host.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 ￼sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv- keys 58118E89F3A912897C070ADBF76221572C52609D
 
 # install and run 'apt-add-repository'
-￼￼sudo apt-get install -y software-properties-common python-software-properties
-￼￼￼sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-￼￼￼sudo apt-get update
+sudo apt-get install -y software-properties-common python-software-properties
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
 
 # use docker-engine repository as default
 apt-cache policy docker-engine
