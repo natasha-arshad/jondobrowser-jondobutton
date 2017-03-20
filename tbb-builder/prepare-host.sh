f1d5c90bd8b19b8768550d39bb77f2ec02f2216d
Oliver <javadoc@email.de>
2017-03-20 21:25:56 +0100

    another PDF formatting

--- tbb-builder/prepare-host.sh
+++ tbb-builder/prepare-host.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 
 # install docker repository key
-￼sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv- keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv- keys 58118E89F3A912897C070ADBF76221572C52609D
 
 # install and run 'apt-add-repository'
 sudo apt-get install -y software-properties-common python-software-properties
